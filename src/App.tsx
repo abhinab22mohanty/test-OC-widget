d2d160c761fbb87b55350ef6d9442d6969996939
Abhinab Mohanty <amohanty@microsoft.com>
2022-04-28 13:58:52 -0700

    Creating test OC app

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,52 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { LiveChatWidget } from "@microsoft/omnichannel-chat-widget";
+import { OmnichannelChatSDK } from "@microsoft/omnichannel-chat-sdk";
+import { useState, useEffect } from 'react';
+
 
 function App() {
+  const [liveChatWidgetProps, setLiveChatWidgetProps] = useState<any>();
+
+  useEffect(() => {
+    const init = async () => {
+        const omnichannelConfig = {
+          orgId: "bfd9d87a-79b7-4bb7-acf0-48b3b855b4da", // dummy config
+          orgUrl: "https://orgf4d9cd3e-crm.omnichannelengagementhub.com", // dummy config
+          widgetId: "993b007b-670c-48a6-8e80-938fccc087be" // dummy config
+      };
+      const chatSDK = new OmnichannelChatSDK(omnichannelConfig);
+      await chatSDK.initialize(); // mandatory
+      const chatConfig = await chatSDK.getLiveChatConfig();
+
+      const liveChatWidgetProps = {
+        styleProps: {
+            generalStyles: {
+              width: "400px",
+              height: "600px",
+              bottom: "30px",
+              right: "30px"
+          }
+        },
+        headerProps: {
+            controlProps: {
+                hideMinimizeButton: true
+            }
+        },
+        chatSDK: chatSDK, // mandatory
+        chatConfig: chatConfig // mandatory
+    };
+
+      setLiveChatWidgetProps(liveChatWidgetProps);
+    }
+
+    init();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        {liveChatWidgetProps && <LiveChatWidget {...liveChatWidgetProps} />}
+      </div>
     </div>
   );
 }
